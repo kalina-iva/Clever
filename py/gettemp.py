339475bd581ec8717192eefe658b5f154539908a
Kalina Mariia <46577545+kalina-iva@users.noreply.github.com>
2019-04-17 00:50:21 +0500

    Update gettemp.py

--- py/gettemp.py
+++ py/gettemp.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5, GPIO.IN)
 
 if GPIO.input(5) == GPIO.HIGH:
-     with open('E:/temper.txt') as fr:
+     with open('./sensors/temper.txt') as fr:
        for line in fr:
          print(line)
   time.sleep(15)
