637af655173fac40b6b155189c85ad6964c31561
Kalina Mariia <46577545+kalina-iva@users.noreply.github.com>
2019-04-17 00:38:59 +0500

    Update gettemp.py

--- py/gettemp.py
+++ py/gettemp.py
@@ -6,10 +6,8 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5, GPIO.IN)
 
-while True:
-  if GPIO.input(5) == GPIO.HIGH:
-    print("Читаем из файла температуру")
-    with open('E:/temper.txt') as fr:
-      for line in fr:
-        print(line)
+if GPIO.input(5) == GPIO.HIGH:
+     with open('E:/temper.txt') as fr:
+       for line in fr:
+         print(line)
   time.sleep(15)
